9c133851b08ddfab6e0ab17852e477bc2600422e
pepperm <arseniy@medinskiy.net>
2020-03-07 22:06:55 +0300

    aaaa

--- 2D_ray/kekush.c
+++ 2D_ray/kekush.c
@@ -11,7 +11,7 @@ int main(void) {
 	SDL_Window *window;
 
 	SDL_Init(SDL_INIT_VIDEO);
-	window = SDL_CreateWindow("2D_ray", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
+	window = SDL_CreateWindow("Hello, CS50!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
 	SDL_RenderClear(renderer);
